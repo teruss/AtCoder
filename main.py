87bc4887de4ed482f66541d66b26cd0687d18bfd
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2015-05-12 05:53:27 +0900

    Solve

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
-def solve(R, C, K, rc):
-    rows = {}
-    columns = {}
-    for r, c in rc:
-        if not r in rows:
-            rows[r] = 0
-        rows[r] += 1
-        if not c in columns:
-            columns[c] = 0
-        columns[c] += 1
+def check(HS, v):
+    N = len(HS)
+    D = [0] * N
+    for h, s in HS:
+        t = (v - h) / s
+        if t < 0:
+            return False
+        if t < N:
+            D[t] += 1
+    x = 0
+    for t in range(N):
+        x += D[t]
+        if x > t + 1:
+            return False
+    return True
 
-    numColumnsForCandy = {}
-    for v in columns.values():
-        if v not in numColumnsForCandy:
-            numColumnsForCandy[v] = 0
-        numColumnsForCandy[v] += 1
-    numColumnsForCandy[0] = C - len(columns)
+def solve(HS):
+    N = len(HS)
+    p = max(HS, key=lambda (h, s): h + s * (N - 1))
+    l = 0
+    r = p[0] + p[1] * (N - 1) + 1
 
-    res = 0
-    for row in rows:
-        remain = K - rows[row]
-        if remain in numColumnsForCandy:
-            res += numColumnsForCandy[remain]
-
-    numRowsWithNoCandy = R - len(rows)
-    if K in numColumnsForCandy:
-        res += numColumnsForCandy[K] * numRowsWithNoCandy
-
-    for r, c in rc:
-        if rows[r] + columns[c] == K:
-            res -= 1
-        if rows[r] + columns[c] - 1 == K:
-            res += 1
-
-    return res
+    while l + 1 < r:
+        mid = (l + r) / 2
+        if check(HS, mid):
+            r = mid
+        else:
+            l = mid
+    return r
 
 if __name__ == "__main__":
-    R, C, K = map(int, raw_input().split())
-    candies = [tuple(map(int, raw_input().split())) for _ in range(input())]
-    print solve(R, C, K, candies)
+    N = input()
+    HS = [tuple(map(int, raw_input().split())) for _ in range(N)]
+    print solve(HS)
--- test.py
+++ test.py
@@ -1,48 +1,11 @@
 from main import *
 import unittest
-import old
-from random import *
 
 class Test(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(5, solve(3, 5, 3, [(1, 2), (2, 1), (2, 5), (3, 2), (3, 5)]))
-    def test_old(self):
-        self.assertEqual(old.solve(3, 5, 3, [(1, 2), (2, 1), (2, 5), (3, 2), (3, 5)]), solve(3, 5, 3, [(1, 2), (2, 1), (2, 5), (3, 2), (3, 5)]))   
-
-    def test_random(self):
-        R = randint(1, 5)
-        C = randint(1, 5)
-        K = randint(1, R * C)
-        N = randint(1, K)
-        rc = []
-        for i in range(N):
-            r = randint(1, R)
-            c = randint(1, C)
-            while (r, c) in rc:
-                r = randint(1, R)
-                c = randint(1, C)
-            rc.append((r, c))
-        print R, C, K, N, rc
-        self.assertEqual(old.solve(R, C, K, rc), solve(R, C, K, rc))
-
-    @unittest.skip("skip")
-    def test_forever(self):
-        while True:
-            self.test_random()
-
-    def test_y(self):
-        R = 4
-        C = 4
-        K = 1
-        rc = [(2, 3)]
-        self.assertEqual(7, solve(R, C, K, rc))
-
-    def test_x(self):
-        R = 17
-        C = 43
-        K = 2
-        rc = [(47, 15), (21, 21), (5, 17), (23, 16), (32, 49), (4, 5), (11, 7), (45, 27), (19, 49), (46, 6), (9, 9), (23, 2), (37, 8), (1, 41), (10, 33), (10, 31), (46, 50), (26, 44), (14, 9), (47, 29), (27, 43), (49, 47), (45, 45), (29, 36), (11, 19), (31, 17), (22, 44), (19, 5), (3, 9)]
-        self.assertEqual(402, solve(R, C, K, rc))
+        self.assertEqual(23, solve([(5, 6), (12, 4), (14, 7), (21, 2)]))
+    def test_case2(self):
+        self.assertEqual(105, solve([(100, 1), (100, 1), (100, 1), (100, 1), (100, 1), (1, 30)]))
                 
 if __name__ == '__main__':
     unittest.main()
